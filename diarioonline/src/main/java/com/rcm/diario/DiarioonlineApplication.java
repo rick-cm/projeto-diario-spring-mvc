1dae2a1c0a5d378048b0a8ea12428469ee43c614
rcmto <rick.mmartins3@gmail.com>
2019-11-18 21:21:51 -0300

    primeiro commit

---
+++ diarioonline/src/main/java/com/rcm/diario/DiarioonlineApplication.java
@@ -0,0 +1,13 @@
+package com.rcm.diario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioonlineApplication.class, args);
+	}
+
+}
---
+++ diarioonline/src/test/java/com/rcm/diario/DiarioonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcm.diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
